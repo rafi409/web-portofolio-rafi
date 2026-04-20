687b245dc2b0848a82eef8a0858015ffebffe815
rafi409 <azzamrafikhairul.gmail.com>
2026-04-20 21:34:37 +0700

    update terbaru

---
+++ script.js
@@ -0,0 +1,16 @@
+.menu {
+  display: none;
+  background: black;
+  padding: 10px;
+}
+
+.menu a {
+  display: block;
+  color: white;
+  margin: 10px 0;
+}
+
+.menu-btn {
+  font-size: 28px;
+  cursor: pointer;
+}
